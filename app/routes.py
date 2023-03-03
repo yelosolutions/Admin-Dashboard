f1dc51470355d969fbc1c80d2141f536642f863c
jay <ronokamau@gmail.com>
2023-03-03 14:53:58 +0300

    file structure + basic files

---
+++ app/routes.py
@@ -0,0 +1,7 @@
+from app import app
+from flask import render_template, url_for
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template('dashboard.html', title='admin dashboard')
